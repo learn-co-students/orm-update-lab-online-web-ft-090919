fd964bfdd67e54df53d6524876591b17d5b56095
Mike Gaudreau <gaudreaum01@gmail.com>
2019-10-10 12:47:36 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -32,15 +32,17 @@ class Student
   end
 
   def save
-    sql = <<~SQL
-    INSERT INTO students(name, grade)
-    VALUES (?, ?)
-    SQL
-    binding.pry
-    if DB[:conn].execute(sql, self.name, self.grade).empty?
+    if self.id
       self.update
+    else
+      sql = <<~SQL
+      INSERT INTO students(name, grade)
+      VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
   def update
